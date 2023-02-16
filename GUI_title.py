be27e6e5b3ab22e10e57193113a5b69e9253f597
Gashaye Adugna <gasha.como@gmail.com>
2023-02-16 01:46:53 -0800

    GUIP

---
+++ GUI_title.py
@@ -0,0 +1,13 @@
+from tkinter import*
+from tkinter import ttk
+import tkinter as tk
+import tkinter.messagebox
+
+root=Tk()
+titlespace=" "
+root.title(102 * titlespace + "python GUI")
+root.geometry("800x700+300+0")
+root.resizable(width=False, height=False)
+root.title
+
+root.mainloop()
