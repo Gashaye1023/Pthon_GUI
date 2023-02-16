947706826765f1ae1c948703ca33aab4a1997072
Gashaye Adugna <gasha.como@gmail.com>
2023-02-16 06:11:07 -0800

    menuoption

---
+++ optionmenu.py
@@ -0,0 +1,41 @@
+from tkinter import*
+from tkinter import ttk
+import tkinter as tk
+import tkinter.messagebox
+
+root=Tk()
+titlespace= " "
+root.title(102*titlespace + "Python GUI")
+root.geometry("800x600+100+10")
+menubar=Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="New")
+filemenu.add_command(label="Open")
+filemenu.add_command(label="Save")
+filemenu.add_command(label="Save as...")
+filemenu.add_command(label="Close")
+
+filemenu.add_separator()
+
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+editmenu = Menu(menubar, tearoff=0)
+editmenu.add_command(label="Undo")
+
+editmenu.add_separator()
+
+editmenu.add_command(label="Cut")
+editmenu.add_command(label="Copy")
+editmenu.add_command(label="Paste")
+editmenu.add_command(label="Delete")
+editmenu.add_command(label="Select All")
+
+menubar.add_cascade(label="Edit", menu=editmenu)
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="Help Index")
+helpmenu.add_command(label="About...")
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+root.config(menu=menubar)
+
+root.mainloop()
